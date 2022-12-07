11dace7388ab00bfaa507e3bab3cb1ceb801f68f
Francisco Sintra <al25048@gmail.com>
2022-12-07 11:27:04 +0000

    fr 1

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,22 @@ namespace MarcelaEChico
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+            
+            
+            this.Show();
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
     }
 }
